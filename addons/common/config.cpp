06e98a3ae81d50f1e2a2cf0847a636c0123580a6
Jonathan Pereira <jonathan.pereira@gmail.com>
2016-01-22 23:43:49 -0200

    Commit Inicial

---
+++ addons/common/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class aegis_server
+	{
+		requiredVersion = 0.1;
+		requiredAddons[] = {"ace_main","CBA_XEH"};
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+	};
+};
